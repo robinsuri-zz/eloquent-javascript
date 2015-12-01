a9d50fc127d349c56c6ef49f81488ba40584773b
robinsuri <robin.suri@gmail.com>
2015-12-01 14:14:49 +0530

    range using ternary operator
    array to list using slice

--- Chapters/4/Q1.js
+++ Chapters/4/Q1.js
@@ -1,17 +1,16 @@
-var range = function(start, end, step)
-{
-    var array = [];
-    if (step > 0) {
-        for (var i = start; i <= end; i += step)
-            array.push(i);
-    }
-    if (step < 0)
-    {
-        for (var i = start; i >= end; i += step)
-            array.push(i);
-    }
-    return array;
-}
+var range = function (start, end, step) {
+  step = (typeof step === "undefined"
+          ? (start < end ? 1 : -1)
+          : step);
+  var isFinished = function (n) {
+    return start < end ? n > end : n < end;
+  }
+  var array = [];
+  for (var n = start; !isFinished(n); n += step) {
+    array.push(n);
+  }
+  return array;
+};
 
 var sum = function(array)
 {
@@ -19,7 +18,7 @@ var sum = function(array)
     var sum = 0;
     for (var i = 0; i < length; i++)
     {
-        sum += array.pop(i);
+        sum += array.[i];
     }
     return sum;
 }
--- Chapters/4/Q3.js
+++ Chapters/4/Q3.js
@@ -1,19 +1,9 @@
 var arrayToList = function(array) {
-    var list = {};
-    var length = array.length - 1;
-    for (var i = length; i >= 0; i--) {
-        if (i == length) {
-            list.value = array[i];
-            list.rest = null;
-        } else {
-            var subList = {};
-            subList.value = array[i];
-            subList.rest = list;
-            list = subList;
-        }
-    }
-    return list;
+  if(array.length==0)
+    return null;
+    return prepend(array[0],arrayToList(array.slice(1)));
 }
+
 
 var listToArray = function(list) {
     var array = [];
@@ -32,19 +22,15 @@ var prepend = function(elem, list) {
 }
 
 var nth = function(list, num) {
+     
     var array = listToArray(list);
     return array[num];
 }
 
 var recurNth = function(list, num) {
-    var newList = recurNthList(list, num);
-    return newList.value;
-}
-
-var recurNthList = function(list, num) {
+   if(list == null)
+    return undefined;
     if (num == 0)
-        return list;
-    var newlist = recurNthList(list, num - 1);
-    return newlist.rest;
+        return list.value;
+    return recurNth(list.rest,num-1);
 }
-
