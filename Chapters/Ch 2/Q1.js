009fae0fe42ea07051c359aeb42ba575ea6060a3
robinsuri <robinsuri@gmail.com>
2017-06-27 16:42:15 +0530

    sdfasf

--- Chapters/Ch 2/Q1.js
+++ Chapters/Ch 2/Q1.js
@@ -1,4 +1,4 @@
-aksdfhjaskdfjassdlfjsdkfvar var a = "" , var s = "";
+robin aksdfhjaskdfjassdlfjsdkfvar var a = "" , var s = "";
 var a="asldfkj";
 for (var num = 1; num <= 7; num += 1) {
     s = s + "#";
