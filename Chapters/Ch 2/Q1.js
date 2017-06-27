922d45db56d83c9873f5fc2c4f33be15ce411162
robinsuri <robinsuri@gmail.com>
2017-06-27 16:46:02 +0530

    sdfsf

--- Chapters/Ch 2/Q1.js
+++ Chapters/Ch 2/Q1.js
@@ -1,4 +1,4 @@
-robin aksdfhjaskdfjassdlfjsdkfvar var a = "" , var s = "";
+asdflkjrobin aksdfhjaskdfjassdlfjsdkfvar var a = "" , var s = "";
 var a="asldfkj";
 for (var num = 1; num <= 7; num += 1) {
     s = s + "#";
