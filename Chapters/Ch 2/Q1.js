aa62d1e839908d682d94f1dd5cdcb9cc3f675a90
robinsuri <robinsuri@gmail.com>
2017-07-12 19:22:01 +0530

    kljlkj

--- Chapters/Ch 2/Q1.js
+++ Chapters/Ch 2/Q1.js
@@ -1,4 +1,4 @@
-sdlfkjsdlkfjsldfkjasdflkjrobin aksdfhjaskdfjassdlfjsdkfvar var a = "" , var s = "";
+alsdkfjslksdlfkjsdlkfjsldfkjasdflkjrobin aksdfhjaskdfjassdlfjsdkfvar var a = "" , var s = "";
 var a="asldfkj";
 for (var num = 1; num <= 7; num += 1) {
     s = s + "#";
