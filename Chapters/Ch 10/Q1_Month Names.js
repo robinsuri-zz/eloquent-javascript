e462a91ef2258361febf3640abcca739fe2013fe
robinsuri <robinsuri@gmail.com>
2017-07-24 19:55:02 +0530

    lkjlkjlk
    ljkjlkj

--- Chapters/Ch 10/Q1_Month Names.js
+++ Chapters/Ch 10/Q1_Month Names.js
@@ -1,6 +1,6 @@
 var month = function() {
 var monthNames = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dev"];
-return { name : function(num) {return monthNames[num];},
+return { name : function(num) {return monthNames[num];},lkdfjsdlkfj
          number : function(name) {return monthNames.indexOf(name);}
        };
 }();
